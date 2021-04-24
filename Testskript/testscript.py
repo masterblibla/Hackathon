277e216d318c6a247735b38d80bc0e5221a80030
André Meyer <andrealexander.meyer@outloook.com>
2021-04-24 17:12:32 +0200

    update André

--- Testskript/testscript.py
+++ Testskript/testscript.py
@@ -11,24 +11,87 @@ def read_xml(path):
     
     tree = ET.parse(path)
     root = tree.getroot()
-    nb_children = len(root.getchildren())
-    print(nb_children)
 
-def search(eintrag, testen, ergebnis = []):
-      #search(input) for testen
-      for i in range(len(eintrag)):
-          if eintrag[i]['tag'] == testen:
-              ergebnis.append([eintrag[i]["tag"], eintrag[i]["possible_type"]])
-              break
-          else: 
-              ergebnis = search(eintrag[i]["children"], testen)
-          
-      return ergebnis
+    return root
+
+def analyse_xml(root):
+    
+    # tag_list = search_document(root)
+    tag_list = [      ["City", [1,8,8]]        ]
+    
+    
+    critical_tags_list = []
+    
+    for tag in tag_list:
+       
+    
+        critical_tag = search(p100_tag_list, tag[0], location = tag[1])
+        
+        critical_tags_list.append(critical_tag)
+    return critical_tags_list
+
+
+def search(p_list, tag, location, ergebnis = [], parents = []):
+    '''
+    recursive in depth search of defined critical tags
+
+    Parameters
+    ----------
+    p_list : list
+        list containing the user defnined critical tags.
+    tag : string
+        string that needs to be checked. 
+    ergebnis : list, optional
+        contains informatioon, if the tag matches one of the critical tags. The default is [].
+
+    Returns
+    -------
+    ergebnis : list
+        contains all matched critical tags and posssible data-types.
+
+    Acces
+    -------
+    ergebnis = [['tag',[possible_type1, possible_type2]]]
+    '''
+    
+    #search(input) for tag
+    
+    for i in range(len(p_list)):
+        # parents.append(p_list[i]['tag'])
+        if p_list[i]['tag'] == tag:
+            ergebnis.append( {'tag': p_list[i]["tag"], 'location_xml': location, 'possible_type': p_list[i]["possible_type"]} )
+           
+        if p_list[i]['children'] != None: 
+            ergebnis = search(p_list[i]["children"], tag, location, parents)
+    
+    return ergebnis
+
+
+
         
 def search_document(eintrag, testen, ergebnis = []):
+    '''
+    searches document for all contained tags
+
+    Parameters
+    ----------
+    eintrag : TYPE
+        DESCRIPTION.
+    testen : TYPE
+        DESCRIPTION.
+    ergebnis : TYPE, optional
+        DESCRIPTION. The default is [].
+
+    Returns
+    -------
+    eintrag : list
+        contains all tags from xml-document.
+
+    '''
+    
     pass
 
-#hallo André
+
 
 #%% 
 #tags which dont contain dsgvo
@@ -74,27 +137,45 @@ p100_tag_list = [
                                {"tag": "City",
                                 "possible_type": ["Stadt"],
                                 "children": None,},
+                               
+                               ],
+                           },
+                          {"tag": "City",
+                           "possible_type": ["Addresse"],
+                           "children": [
+                               {"tag": "Name1",
+                                "possible_type": ["Name"],
+                                "children": None,},
+                               {"tag": "Street",
+                                "possible_type": ["Straße"],
+                                "children": None,},
+                               {"tag": "PCode",
+                                "possible_type": ["Postleitzahl"],
+                                "children": None,},
+                               {"tag": "City",
+                                "possible_type": ["Gebäudename"],
+                                "children": None,},
+                               
                                ],
                            },],
                       },],
                  },]
 
-eg_dict_safe_list = {
-    "tag": "LblPrj",
-    "in_combination_with": "ProjName",
-    "possible_type": "firma",
-    }
+# eg_dict_safe_list = {
+#     "tag": "LblPrj",
+#     "possible_type": "firma",
+#     "children": [...]
+#     }
 #%%
 
 if __name__ == "__main__":
     
-    example_xml = '271_Test.X82'
+    # example_xml = '271_Test.X82'
+    # root = read_xml(example_xml)
     
-    read_xml(example_xml)
+    # critical_tags_list = analyse_xml(root)
     
-    # eintrag = p100_tag_list
-    # testen = "Descrip"
+    critical_tags_list = search(p100_tag_list,  "City", [1,8,8])
     
-    # wert = search(eintrag, testen)
-    # pprint.pprint(wert)
+    pprint.pprint(critical_tags_list)
   
